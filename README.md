07f2cd86efaadca476d53975b6994016b8959f1e
fanhl <dyguests@qq.com>
2018-09-16 20:43:56 +0800

    非主线程更新UI

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A project for Android message communication learn.
 # Base
 
 [Android中的Thread, Looper和Handler机制(附带HandlerThread与AsyncTask)](https://hit-alibaba.github.io/interview/Android/basic/Android-handler-thread-looper.html)
+
+[Android Handler与Looper原理简析](https://juejin.im/post/59083d7fda2f60005d14efdb)
--- app/src/main/java/com/fanhl/msgcommunilearn/ui/looper2/Looper2Activity.kt
+++ app/src/main/java/com/fanhl/msgcommunilearn/ui/looper2/Looper2Activity.kt
@@ -1,12 +1,14 @@
 package com.fanhl.msgcommunilearn.ui.looper2
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.os.Message
+import android.os.*
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.fanhl.msgcommunilearn.R
+import kotlinx.android.synthetic.main.activity_looper2.*
 
+/**
+ * 非主线程更新UI
+ */
 class Looper2Activity : AppCompatActivity() {
     private lateinit var myThread: MyThread
 
@@ -14,17 +16,39 @@ class Looper2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_looper2)
 
+        myThread = MyThread().apply {
+            start()
+        }
 
-        myThread = MyThread()
+        button.setOnClickListener {
+            myThread.handler.sendMessage(Message().apply {
+                what = 1
+                obj = "haaa"
+            })
+        }
+
+        button2.setOnClickListener {
+            myThread.handler.post {
+                try {
+                    textView.text = "${System.currentTimeMillis()}"
+                } catch (e: Exception) {
+                } finally {
+                }
+            }
+        }
+    }
+
+    companion object {
+        val TAG = Looper2Activity::class.java.simpleName!!
     }
 
     class MyThread : HandlerThread("Looper2") {
         lateinit var handler: Handler
 
         override fun onLooperPrepared() {
-            handler = object : Handler() {
+            handler = object : Handler(Looper.myLooper()) {
                 override fun handleMessage(msg: Message?) {
-
+                    Log.d(TAG, "获得了message ${msg?.obj}")
                 }
             }
         }
